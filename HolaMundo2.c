fa65d2ab8b4a7a9539ec4f381713598727e5ad57
AlexisCoteValencia <72525011+AlexisCoteValencia@users.noreply.github.com>
2020-10-07 16:53:56 -0500

    Agregué mi nombre crack

--- HolaMundo2.c
+++ HolaMundo2.c
@@ -4,5 +4,6 @@ printf("\nFernando Julian Diaz Vazquez\n");
 printf("Este es mi segundo codigo");
 printf("Programar es facil");
 printf("Voy a compartir con mi compañero el codigo");
+printf("\nCote Valencia Alexis Daniel\n");
 return 0;
 }
